f8bdc17a3081d2e398d7e43e4e4ba5b27101288b
Didier <ddarricau@alkemics.com>
2022-06-22 09:22:19 +0200

    Add deallocate

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Set
 
 
 @dataclass(frozen=True)
@@ -12,10 +13,27 @@ class Batch:
     def __init__(self, ref: str, sku: str, qty: int):
         self.ref = ref
         self.sku = sku
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - sum([line.qty for line in self._allocations])
 
     def allocate(self, order_line: OrderLine):
-        self.available_quantity -= order_line.qty
+        if self.can_allocate(order_line):
+            self._allocations.add(order_line)
 
-    def can_allocate(self, order_line: OrderLine):
-        return self.sku == order_line.sku and self.available_quantity >= order_line.qty
+    def can_allocate(self, order_line: OrderLine) -> bool:
+        return (
+            self.sku == order_line.sku
+            and self.available_quantity >= order_line.qty
+            and order_line not in self._allocations
+        )
+
+    def deallocate(self, order_line: OrderLine):
+        if self.can_deallocate(order_line):
+            self._allocations.remove(order_line)
+
+    def can_deallocate(self, order_line: OrderLine) -> bool:
+        return order_line in self._allocations
--- test_batches.py
+++ test_batches.py
@@ -15,6 +15,13 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     assert batch.available_quantity == 1
 
 
+def test_cannot_allocate_to_a_batch_with_less_than_requested_quantity():
+    batch, order_line = make_batch_and_line(sku="sku", batch_qty=10, line_qty=11)
+    batch.allocate(order_line)
+
+    assert batch.available_quantity == 10
+
+
 def test_cannot_allocate_if_available_less_than_required():
     batch, order_line = make_batch_and_line(sku="sku", batch_qty=10, line_qty=11)
     result = batch.can_allocate(order_line)
@@ -40,3 +47,23 @@ def test_cannot_allocate_if_sku_does_not_match_in_batch():
     batch = Batch(ref="abatch", sku="asku", qty=10)
     order_line = OrderLine(orderid="anorder", sku="bsku", qty=9)
     assert not batch.can_allocate(order_line)
+
+
+def test_deallocating_an_allocated_line_increases_available_quantity():
+    batch, order_line = make_batch_and_line(sku="sku", batch_qty=10, line_qty=10)
+    batch.allocate(order_line)
+    batch.deallocate(order_line)
+    assert batch.available_quantity == 10
+
+
+def test_can_only_deallocate_allocated_line():
+    batch, unallocated_line = make_batch_and_line(sku="sku", batch_qty=10, line_qty=10)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 10
+
+
+def test_can_allocate_an_order_line_only_once():
+    batch, order_line = make_batch_and_line(sku="sku", batch_qty=20, line_qty=10)
+    batch.allocate(order_line)
+    batch.allocate(order_line)
+    assert batch.available_quantity == 10
